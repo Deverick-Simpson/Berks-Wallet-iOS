d05308bb56ef4eee437a7db454a9a65b6fa6693a
Deverick <deverick@me.com>
2019-08-09 21:15:21 +0200

    Load appointments into profile view

--- Berks Wallet/History/HistoryView.swift
+++ Berks Wallet/History/HistoryView.swift
@@ -6,18 +6,23 @@
 //  Copyright © 2019 ApolloLabs. All rights reserved.
 //
 import UIKit
+import Firebase
+import SwiftDate
+import InteractiveSideMenu
 
-class HistoryView: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class HistoryView: UIViewController, UITableViewDataSource, UITableViewDelegate, SideMenuItemContent, Storyboardable{
 
     let db_api = Data_Access()
     
 
     @IBOutlet var tableView: UITableView!
-        
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
     override func viewDidAppear(_ animated: Bool){
@@ -35,22 +40,37 @@ class HistoryView: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-  
        return db_api.userHistoryCount()
-        
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReceiptTableViewCell") as! ReceiptTableViewCell
 
         let text = db_api.arrayOfClientHistory[indexPath.row]
 
-        //add last name
-        cell.barber_name.text = text.first_name
+        //Cell First and Last name
+        cell.barber_name.text = text.first_name + text.last_name
+        
+        //Cell Total Amount
+        var totalString = String(format: "%f", text.amount)
+        cell.receipt_amount.text = totalString
+        
+//        text.date.dateValue()
+        
+        
+//        let dateformatter = DateFormatter()
+//
+//        dateformatter.dateStyle = DateFormatter.Style.short
+//
+//        dateformatter.timeStyle = DateFormatter.Style.short
 
+//        let dateString = dateformatter.string(from: text.date.dateValue())
+        //cell.date.text = dateString
+        
+        
         return cell
         
         
--- Berks Wallet/New Group1/MainMenu.swift
+++ Berks Wallet/New Group1/MainMenu.swift
@@ -17,21 +17,77 @@
 //
 
 import UIKit
+import Firebase
 import InteractiveSideMenu
 
 /**
  SideMenu is a controller relevant one of the side menu items. To indicate this it adopts `SideMenuItemContent` protocol.
  */
-class MainMenu: UIViewController, SideMenuItemContent, Storyboardable {
+class MainMenu: UIViewController, UITableViewDataSource, UITableViewDelegate, SideMenuItemContent, Storyboardable {
 
+    let db_api = Data_Access()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
-    // Show side menu on menu swipe right
-    @IBAction func openMenu(_ sender: UIButton) {
-        showSideMenu()
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        db_api.buildUserHistoryList(completion: printholder)
+        // This view controller itself will provide the delegate methods and row data for the table view.
     }
+    
+    func printholder(){
+        print("Done building user history list")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return db_api.userHistoryCount()
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReceiptTableViewCell") as! ReceiptTableViewCell
+        
+        let text = db_api.arrayOfClientHistory[indexPath.row]
+        
+        //Cell First and Last name
+        cell.barber_name.text = text.first_name + text.last_name
+        
+        //Cell Total Amount
+        var totalString = String(format: "%f", text.amount)
+        cell.receipt_amount.text = totalString
+        
+        //        text.date.dateValue()
+        
+        
+        //        let dateformatter = DateFormatter()
+        //
+        //        dateformatter.dateStyle = DateFormatter.Style.short
+        //
+        //        dateformatter.timeStyle = DateFormatter.Style.short
+        
+        //        let dateString = dateformatter.string(from: text.date.dateValue())
+        //cell.date.text = dateString
+        
+        
+        return cell
+        
+        
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+
     
     @IBAction func Schedule_Action(_ sender: Any) {
         
